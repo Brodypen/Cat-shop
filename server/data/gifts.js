ad383c62751dd58043e8511467552f30211098b2
Brodypen <brodypen7987@gmail.com>
2023-09-22 15:19:50 -0500

    Cat shop people

--- server/data/gifts.js
+++ server/data/gifts.js
@@ -5,8 +5,8 @@ const giftData = [
     age: 3,
     tags: "Brown, black, and white kitty",
     image: "https://bloximages.newyork1.vip.townnews.com/dnronline.com/content/tncms/assets/v3/editorial/e/f7/ef7b4f51-bef9-5b2e-bfc3-57d9acabf84b/5f2b18e7bf895.image.jpg?resize=1200%2C900",
-    description: "Description 1",
-    submittedBy: "User 1",
+    description: "Disco is a playful 3-year-old kitty with a coat of brown, black, and white. He loves to chase after toys and explore new spaces. He's sure to bring a lot of joy to your home!",
+    submittedBy: "John Smith",
     submittedOn: "2018-01-01",
   },
   {
@@ -15,8 +15,8 @@ const giftData = [
     age: 2,
     tags: "Gray and white kitty",
     image: "https://tapinto-production.s3.amazonaws.com/uploads/articles/be/best_crop_91f0686b0bab05aed7f4_best_crop_6e9be72e94eeb91ef200_297011959_10162582373797837_3451879443687791904_n.jpeg?id=4481304",
-    description: "Description 2",
-    submittedBy: "User 2",
+    description: "Meet Whiskers, a charming 2-year-old kitty with a sleek coat of gray and white fur. He's known for his playful antics and affectionate nature. Whiskers will make a wonderful addition to any loving home!",
+    submittedBy: "Jane Doe",
     submittedOn: "2018-02-15",
   },
   {
@@ -25,8 +25,8 @@ const giftData = [
     age: 1,
     tags: "Fluffy black and white kitty",
     image: "https://www.iberkshires.com/images/feature/1574365546.jpg",
-    description: "Description 3",
-    submittedBy: "User 3",
+    description: "Mittens is a fluffy 1-year-old kitty with striking black and white markings. She's as sweet as can be and loves cuddling up in warm laps. Bring Mittens into your home and experience the joy of her gentle purrs!",
+    submittedBy: "James Brown",
     submittedOn: "2018-03-23",
   },
   {
@@ -35,8 +35,8 @@ const giftData = [
     age: 4,
     tags: "White kitty",
     image: "https://www.woodlandsonline.com/images/archive/cat_show_1.JPG",
-    description: "Description 4",
-    submittedBy: "User 4",
+    description: "Oreo is a dignified 4-year-old white kitty with a regal presence. He enjoys basking in the sun and observing the world around him. With his elegant charm, Oreo will be a delightful companion for those seeking a refined feline friend.",
+    submittedBy: "Olivia Wilson",
     submittedOn: "2018-04-10",
   },
   {
@@ -45,8 +45,8 @@ const giftData = [
     age: 2,
     tags: "Brown kitty",
     image: "https://palmettoanimalleague.org/wp-content/uploads/2019/04/PAL_YoYo_1_copy.jpg",
-    description: "Description 5",
-    submittedBy: "User 5",
+    description: "Smokey is a 2-year-old brown kitty with a playful spirit. He's always up for a game of chase and loves exploring nooks and crannies. Smokey's boundless energy and curious nature will bring endless entertainment to your household!",
+    submittedBy: "Michael Johnson",
     submittedOn: "2018-05-05",
   }
 ];
